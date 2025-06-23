5362802923d442abba52b30df78a02a5698188b8
elishalight <87694147+elishalight@users.noreply.github.com>
2025-06-23 11:46:26 +0300

    Delete custom_components/RED_ALERT_By_Elisha_Design directory

--- custom_components/RED_ALERT_By_Elisha_Design/const.py
+++
@@ -1,4 +0,0 @@
-DOMAIN = "red_alert"
-LICENSES_URL = "https://raw.githubusercontent.com/elishalight/Elisha-Design-Privete/refs/heads/main/Passcodes/Red_Alert_App"
-OREF_ALERTS_URL = "https://www.oref.org.il/WarningMessages/alert/alerts.json"
-OREF_CITIES_URL = "https://alerts-history.oref.org.il/Shared/Ajax/GetCitiesMix.aspx"
